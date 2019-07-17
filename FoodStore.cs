0bbe75d2da07065392401f2b529c111b76b8b50b
Daniel Clarke <deliotclarke@gmail.com>
2019-07-17 14:55:40 -0500

    finished demo

--- FoodStore.cs
+++ FoodStore.cs
@@ -2,8 +2,17 @@ using System.Collections.Generic;
 
 namespace TheMall
 {
-    public class FoodStore
+    public class FoodStore : IStore<FoodItem>
     {
         private Dictionary<string, FoodItem> _inventory = new Dictionary<string, FoodItem>();
+        public void AddToInventory(string name, FoodItem item)
+        {
+            _inventory.Add(name, item);
+        }
+
+        public FoodItem GetFromInventory(string name)
+        {
+            return _inventory[name];
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace TheMall
 {
     class Program
@@ -7,7 +9,34 @@ namespace TheMall
         {
             DollarStore Dollarama = new DollarStore();
 
+            DollarItem MultiColorPen = new DollarItem()
+            {
+                Name = "Muli Colored Pen with 12 colors"
+            };
+
+            Dollarama.AddToInventory("mcp", MultiColorPen);
+
             FoodStore Cinnabon = new FoodStore();
+
+            FoodItem ClassicRoll = new FoodItem()
+            {
+                Name = "Classic Roll",
+                Price = 3.50,
+                Calories = 880
+            };
+
+            FoodItem CaramelPecanBun = new FoodItem()
+            {
+                Name = "Caramel Pecan Bun",
+                Price = 6,
+                Calories = 1080
+            };
+
+            Cinnabon.AddToInventory("tcr", ClassicRoll);
+            Cinnabon.AddToInventory("cpb", CaramelPecanBun);
+
+            Console.WriteLine(Dollarama.GetFromInventory("mcp").Name);
+            Console.WriteLine(Cinnabon.GetFromInventory("cpb").Name);
         }
     }
 }
